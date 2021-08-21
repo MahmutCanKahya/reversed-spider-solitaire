e140b2f9bf0f3d1f692d6a24eed8ee419bed876b
Mahmut Can Kahya <mahmutcankahya@windowslive.com>
2021-08-22 01:07:25 +0300

    cypress test completed

--- cypress/integration/spider-solitaire-game.spec.ts
+++ cypress/integration/spider-solitaire-game.spec.ts
@@ -14,6 +14,10 @@ describe("gamepage-tests", () => {
     currentUrl.should("equal", SPIDER_SOLITAIRE_GAME_PAGE);
   });
 
+  it("should have title REVERSED SPIDER SOLITAIRE on navbar", () => {
+    cy.get(".title").should("contain", "REVERSED SPIDER SOLITAIRE");
+  });
+
   it("should change border color red when dragenter event", () => {
     const card = cy
       .get(".onesuite > :nth-child(4) > :nth-last-child(1)")
@@ -40,12 +44,37 @@ describe("gamepage-tests", () => {
       .should("have.css", "transform", "matrix(1.1, 0, 0, 1.1, 0, 0)");
   });
 
-  it("should each decks size increase when clicked remainder cards", () => {
-    cy.get(".remainderCard").each((i) => {
-      console.log(i);
-    });
+  it("should remainder cards size decrease when clicked remainder cards", () => {
     cy.get(".card__remcards").its("length").should("equal", 5);
     cy.get(".card__remcards").last().trigger("click");
     cy.get(".card__remcards").its("length").should("equal", 4);
+    cy.get(".card__remcards").last().trigger("click");
+    cy.get(".card__remcards").its("length").should("equal", 3);
+    cy.get(".card__remcards").last().trigger("click");
+    cy.get(".card__remcards").its("length").should("equal", 2);
+    cy.get(".card__remcards").last().trigger("click");
+    cy.get(".card__remcards").its("length").should("equal", 1);
+    cy.get(".card__remcards").last().trigger("click");
+    cy.get(".card__remcards").should("not.exist");
+  });
+
+  it("should change color when clicked hint", () => {
+    cy.get(".hint-text").contains("Hint OFF");
+    cy.get(".hint").trigger("click");
+    cy.get(".hint-text").should("contain", "Hint ON");
+  });
+
+  it("shoud be 10 decks", () => {
+    cy.get(".onesuite > div").should("have.length", 10);
+  });
+  
+  it("should first four deck be 6 card and last six decks 5 card", () => {
+    cy.get(".onesuite > div").each(($li, index, $list) => {
+      var val = 5;
+      if (index < 4) {
+        val = 6;
+      }
+      expect($li.children().length).equal(val);
+    });
   });
 });
